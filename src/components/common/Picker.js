5e16d166bbe663182c728480d29ae72fd510959c
Mansi Shah <sonal@radicalrack.com>
2023-10-10 17:50:41 +0530

    Update Picker.js

--- src/components/common/Picker.js
+++ src/components/common/Picker.js
@@ -1,15 +1,12 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
 import * as _ from 'lodash';
 import PropTypes from 'prop-types';
+import DropDown from 'react-native-paper-dropdown';
 
 import MMStyles from '../../helpers/Styles';
-import MMColors from '../../helpers/Colors';
 import MMFormErrorText from './FormErrorText';
-import DropDown from 'react-native-paper-dropdown';
-import { IconButton } from 'react-native-paper';
-
 
 function MMPicker(props) {
     const [visible, setVisible] = useState(false);
@@ -36,37 +33,11 @@ function MMPicker(props) {
 
 MMPicker.propTypes = {
     label: PropTypes.string,
-    placeholder: PropTypes.string,
     value: PropTypes.any,
     items: PropTypes.any,
-    labelValuePair: PropTypes.bool,
-    labelParameter: PropTypes.string,
-    valueParameter: PropTypes.string,
     onValueChange: PropTypes.func,
     errorMessage: PropTypes.any,
-    enabled: PropTypes.bool,
     optionalStyle: PropTypes.object,
-    optionalStyleText: PropTypes.object,
-    selectedItem: PropTypes.any
 };
-
-const styles = StyleSheet.create({
-    picker: {
-        width: '100%',
-        color: MMColors.label,
-        paddingLeft: 40
-    },
-    pickerIcon: {
-        color: MMColors.orange,
-        position: 'absolute',
-        bottom: 16,
-        zIndex: 999,
-        right: 10,
-        backgroundColor: MMColors.white,
-    },
-    pickerIconDisabled: {
-        color: MMColors.disabled,
-    },
-});
 
 export default MMPicker;
