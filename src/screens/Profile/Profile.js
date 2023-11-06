676f2c4bea987a256b051d89fe73ed48aa2d2f05
Nupur Shah <sonal@radicalrack.com>
2023-11-06 16:12:34 +0530

    logout

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -4,12 +4,23 @@ import _ from 'lodash';
 import PropTypes from 'prop-types';
 
 import MMContentContainer from '../../components/common/ContentContainer';
+import { Button } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 export default function Profile({ route }) {
+    const navigation = useNavigation();
+    const renderView = () => {
+        return (
+            <>
+                <Button onPress={() => navigation.navigate('Logout')}>click</Button>
+            </>
+        );
+    };
 
     return (
         <>
             <MMContentContainer>
+                {renderView()}
             </MMContentContainer>
         </>
     );
