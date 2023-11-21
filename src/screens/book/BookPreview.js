22204ba5a99c788700f98133c5ca5eba6e217989
Nupur Shah <sonal@radicalrack.com>
2023-11-21 11:42:49 +0530

    Update BookPreview.js

--- src/screens/book/BookPreview.js
+++ src/screens/book/BookPreview.js
@@ -4,12 +4,22 @@ import _ from 'lodash';
 import PropTypes from 'prop-types';
 
 import MMContentContainer from '../../components/common/ContentContainer';
+import MMSurface from '../../components/common/Surface';
 
 export default function BookPreview({ route }) {
+
+    const renderView = () => {
+        return (
+            <MMSurface>
+
+            </MMSurface>
+        );
+    };
 
     return (
         <>
             <MMContentContainer>
+                {renderView()}
             </MMContentContainer>
         </>
     );
