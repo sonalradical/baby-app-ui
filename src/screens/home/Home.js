20d3616f5fdb5816174ba1cf9518f6f7f03d1d42
Nupur Shah <sonal@radicalrack.com>
2023-11-21 12:34:04 +0530

    Update Home.js

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { setBaby } from '../../redux/Slice/AppSlice';
 
+import MMUtils from '../../helpers/Utils';
 import MMApiService from '../../services/ApiService';
 import MMContentContainer from '../../components/common/ContentContainer';
 import ChapterList from '../chapter/ChapterList';
@@ -31,6 +32,7 @@ export default function Home() {
         const response = await MMApiService.babyList();
         if (response.data) {
             const babyProfiles = response.data;
+            MMUtils.setItemToStorage(MMEnums.storage.selectedBaby, babyProfiles[0]._id);
             dispatch(setBaby(babyProfiles[0]._id));
         }
     }
