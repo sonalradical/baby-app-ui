7a44090ba4761bec499c77d4a5787a9874a65868
Nupur Shah <sonal@radicalrack.com>
2023-12-08 11:58:27 +0530

    add navigation

--- src/screens/templates/Column2.js
+++ src/screens/templates/Column2.js
@@ -1,39 +1,86 @@
 import React from 'react';
-import { View, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
 import { useTheme } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
 
 import * as _ from 'lodash';
+import { Svg, Image as SvgImage } from 'react-native-svg';
 
 import MMConstants from '../../helpers/Constants';
 import MMIcon from '../../components/common/Icon';
+import MMSpinner from '../../components/common/Spinner';
 
 const Column2 = (props) => {
     const theme = useTheme();
-    const { onPickImage, borderWidth = 1, templateData, isDisable = false } = props;
+    const navigation = useNavigation();
+    const { onPickImage, templateData, pageId, templateName, isDisable = false } = props;
+
+    const calculateScaleFactor = (templateName) => {
+        const template = templateData.find((item) => item.name === templateName);
+
+        if (!template || !template.imageParam || !template.imageParam.width) {
+            return 1;
+        }
+
+        const widthScale = (Dimensions.get('window').width / 2) / template.imageParam.width;
+        const heightScale = (Dimensions.get('window').width) / template.imageParam.height;
+
+        return _.min([widthScale, heightScale]);
+    };
+
+    const scaleFactor1 = calculateScaleFactor('p1');
+    const scaleFactor2 = calculateScaleFactor('p2');
+
+    const renderImage = (name, scaleFactor) => {
+        const template = templateData.find(item => item.name === name);
+
+        if (template) {
+            return (
+                <Svg height={Dimensions.get('window').width} width={Dimensions.get('window').width / 2}>
+                    {template.source ?
+                        <SvgImage
+                            href={template?.source}
+                            preserveAspectRatio="xMidYMid slice"
+                            clipPath="url(#clip)"
+                            x={template.imageParam?.x / 2}
+                            y={template.imageParam?.y / 2}
+                            width={template.imageParam?.width * scaleFactor * template.imageParam?.scale}
+                            height={template.imageParam?.height * scaleFactor * template.imageParam?.scale}
+                        /> : <MMSpinner />}
+                </Svg>
+            );
+        } else {
+            return <MMIcon iconName={'plus-circle'} style={styles(theme).imagePickerButton} />;
+        }
+    };
+
+    const renderImageBox = (name, scaleFactor, pageId, extraStyle = {}) => {
+        const onPressHandler = pageId
+            ? () => navigation.navigate('CommonShapes', { shapeName: 'Column', templateData: templateData.find(item => item.name === name), templateName })
+            : () => onPickImage(name, 'img', 'Column');
+
+        return (
+            <TouchableOpacity style={[styles(theme).column, extraStyle]} onPress={onPressHandler} disabled={isDisable}>
+                {renderImage(name, scaleFactor)}
+            </TouchableOpacity>
+        );
+    };
 
     return (
-        <View style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            flex: 1
-        }}>
-            <TouchableOpacity style={[styles(theme).column, { borderRightWidth: borderWidth, borderColor: theme.colors.outline }]}
-                onPress={() => onPickImage('p1', 'img')} disabled={isDisable}>
-                {templateData.some(item => item.name === 'p1') ? <Image source={{ uri: templateData.find(item => item.name === 'p1').source }}
-                    style={styles(theme).image} /> :
-                    <MMIcon iconName={'plus-circle'} style={styles(theme).imagePickerButton} />}
-            </TouchableOpacity>
-
-            <TouchableOpacity style={styles(theme).column} onPress={() => onPickImage('p2', 'img')} disabled={isDisable}>
-                {templateData.some(item => item.name === 'p2') ? <Image source={{ uri: templateData.find(item => item.name === 'p2').source }}
-                    style={styles(theme).image} /> :
-                    <MMIcon iconName={'plus-circle'} style={styles(theme).imagePickerButton} />}
-            </TouchableOpacity>
-        </View>
+        <>
+            <View style={styles(theme).row}>
+                {renderImageBox('p1', scaleFactor1, pageId, { borderRightWidth: 1, borderColor: theme.colors.outline })}
+                {renderImageBox('p2', scaleFactor2, pageId)}
+            </View>
+        </>
     );
 };
 
 const styles = (theme) => StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: 'row', // main axis
+    },
     column: {
         flex: 1,
         alignItems: 'center',
@@ -43,12 +90,7 @@ const styles = (theme) => StyleSheet.create({
     imagePickerButton: {
         padding: MMConstants.paddingLarge,
         borderRadius: 50
-    },
-    image: {
-        width: '100%',
-        height: 345,
-        resizeMode: 'cover'
-    },
+    }
 });
 
 export default Column2;
--- src/screens/templates/Row2.js
+++ src/screens/templates/Row2.js
@@ -1,318 +1,96 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { View, StyleSheet, Dimensions, PanResponder, Animated } from 'react-native';
-import { useTheme, IconButton, Text } from 'react-native-paper';
-import _ from 'lodash';
+import React from 'react';
+import { View, StyleSheet, TouchableOpacity, Dimensions } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import { useNavigation } from '@react-navigation/native';
+
+import * as _ from 'lodash';
+import { Svg, Image as SvgImage } from 'react-native-svg';
+
 import MMConstants from '../../helpers/Constants';
 import MMIcon from '../../components/common/Icon';
-import Svg, { Image as SvgImage } from 'react-native-svg';
-import { PinchGestureHandler, State } from 'react-native-gesture-handler';
+import MMSpinner from '../../components/common/Spinner';
 
 const Row2 = (props) => {
     const theme = useTheme();
-    const { onPickImage, templateData, borderWidth = 1, isDisable = false, onSetTemplateData } = props;
+    const navigation = useNavigation();
+    const { onPickImage, templateData, pageId, templateName, isDisable = false } = props;
 
-    const [position1, setPosition1] = useState({ x: 0, y: 0 });
-    const [position2, setPosition2] = useState({ x: 0, y: 0 });
-    const [scale1, setScale1] = useState(1);
-    const [scale2, setScale2] = useState(1);
 
-    const baseScaleRef1 = useRef(1);
-    const pinchRef1 = useRef(null);
+    const calculateScaleFactor = (templateName) => {
+        const template = templateData.find((item) => item.name === templateName);
 
-    const baseScaleRef2 = useRef(1);
-    const pinchRef2 = useRef(null);
-
-    const scaleFactor1 = templateData && templateData.find((item) => item.name === 'p1')?.width
-        ? _.min([
-            Dimensions.get('window').width / templateData.find((item) => item.name === 'p1').width,
-            170 / templateData.find((item) => item.name === 'p1').height,
-        ])
-        : 1;
-
-    const scaleFactor2 = templateData && templateData.find((item) => item.name === 'p2')?.width
-        ? _.min([
-            Dimensions.get('window').width / templateData.find((item) => item.name === 'p2').width,
-            170 / templateData.find((item) => item.name === 'p2').height,
-        ])
-        : 1;
-
-    useEffect(() => {
-        if (templateData.find((item) => item.name === 'p1')?.width) {
-            setPosition1({ x: templateData.find((item) => item.name === 'p1').x, y: templateData.find((item) => item.name === 'p1').y });
-            setScale1(templateData.find((item) => item.name === 'p1').scale)
+        if (!template || !template.imageParam || !template.imageParam.width) {
+            return 1;
         }
-    }, [templateData.find((item) => item.name === 'p1')?.width]);
 
+        const widthScale = (Dimensions.get('window').width - 20) / template.imageParam.width;
+        const heightScale = (Dimensions.get('window').width / 2) / template.imageParam.height;
 
-    useEffect(() => {
-        if (templateData.find((item) => item.name === 'p2')?.width) {
-            setPosition2({ x: templateData.find((item) => item.name === 'p2').x, y: templateData.find((item) => item.name === 'p2').y });
-            setScale2(templateData.find((item) => item.name === 'p2').scale)
+        return _.min([widthScale, heightScale]);
+    };
+
+    const scaleFactor1 = calculateScaleFactor('p1');
+    const scaleFactor2 = calculateScaleFactor('p2');
+
+    const renderImage = (name, scaleFactor) => {
+        const template = templateData.find(item => item.name === name);
+
+        if (template) {
+            return (
+                <Svg height={Dimensions.get('window').width / 2} width={Dimensions.get('window').width - 20}>
+                    {template.source ?
+                        <SvgImage
+                            href={template?.source}
+                            preserveAspectRatio="xMidYMid slice"
+                            clipPath="url(#clip)"
+                            x={template.imageParam?.x / 2}
+                            y={template.imageParam?.y / 2}
+                            width={template.imageParam?.width * scaleFactor * template.imageParam?.scale}
+                            height={template.imageParam?.height * scaleFactor * template.imageParam?.scale}
+                        /> : <MMSpinner />}
+                </Svg>
+            );
+        } else {
+            return <MMIcon iconName={'plus-circle'} style={styles(theme).imagePickerButton} />;
         }
-    }, [templateData.find((item) => item.name === 'p2')?.width]);
-
-    useEffect(() => {
-        if (templateData, onSetTemplateData) {
-            templateData.map((data, index) => {
-                if (data.name === 'p1') {
-                    data.x = position1.x,
-                        data.y = position1.y,
-                        data.scale = scale1
-                } else if (data.name === 'p2') {
-                    data.x = position2.x,
-                        data.y = position2.y,
-                        data.scale = scale2
-                }
-            })
-            onSetTemplateData(templateData);
-        }
-    }, [position1, position2, scale1, scale2]);
-
-    const handlePinch1 = event => {
-        const { focalX, focalY, scale: pinchScale } = event.nativeEvent;
-        const newScale = baseScaleRef1.current * pinchScale;
-
-        // Calculate the position adjustment based on the focal point and new scale
-        const focalXPercentage = (focalX - position1.x) / (templateData.find((item) => item.name === 'p1').width * scaleFactor1 * scale1);
-        const focalYPercentage = (focalY - position1.y) / (templateData.find((item) => item.name === 'p1').height * scaleFactor1 * scale1);
-
-        // Calculate the new position
-        const newPosX = focalX - focalXPercentage * (templateData.find((item) => item.name === 'p1').width * scaleFactor1 * newScale);
-        const newPosY = focalY - focalYPercentage * (templateData.find((item) => item.name === 'p1').height * scaleFactor1 * newScale);
-
-        // Update the scale and position
-        setScale1(newScale);
-        setPosition1({ x: newPosX, y: newPosY });
     };
 
-    const handlePinchEnd1 = () => {
-        baseScaleRef1.current = scale1;
+    const renderImageBox = (name, scaleFactor, pageId, extraStyle = {}) => {
+        const onPressHandler = pageId
+            ? () => navigation.navigate('CommonShapes', { shapeName: 'Row', templateData: templateData.find(item => item.name === name), templateName })
+            : () => onPickImage(name, 'img', 'Row');
+
+        return (
+            <TouchableOpacity style={[styles(theme).row, extraStyle]} onPress={onPressHandler} disabled={isDisable}>
+                {renderImage(name, scaleFactor)}
+            </TouchableOpacity>
+        );
     };
-
-    const handlePinch2 = event => {
-        const { focalX, focalY, scale: pinchScale } = event.nativeEvent;
-        const newScale = baseScaleRef2.current * pinchScale;
-
-        const minScale = 1; // Adjust this value to set the minimum zoom level.
-        const maxScale = 4; // Adjust this value to set the maximum zoom level (e.g., 5 means 5 times zoom)
-
-        // Apply constraints to newScale
-        //const constrainedScale = Math.min(Math.max(newScale, minScale), maxScale);
-
-        // Calculate the position adjustment based on the focal point and new scale
-        const focalXPercentage = (focalX - position2.x) / (templateData.find((item) => item.name === 'p2').width * scaleFactor2 * scale2);
-        const focalYPercentage = (focalY - position2.y) / (templateData.find((item) => item.name === 'p2').height * scaleFactor2 * scale2);
-
-        // Calculate the new position
-        const newPosX = focalX - focalXPercentage * (templateData.find((item) => item.name === 'p2').width * scaleFactor2 * newScale);
-        const newPosY = focalY - focalYPercentage * (templateData.find((item) => item.name === 'p2').height * scaleFactor2 * newScale);
-
-        // Update the scale and position
-        setScale2(newScale);
-        setPosition2({ x: newPosX, y: newPosY });
-    };
-
-    const handlePinchEnd2 = () => {
-        baseScaleRef2.current = scale2;
-    };
-
-    const panResponder1 = PanResponder.create({
-        onMoveShouldSetPanResponder: (_, gestureState) => {
-            if (gestureState.numberActiveTouches > 1) {
-                return false;
-            }
-            return true;
-        },
-        onPanResponderMove: (_, gestureState) => {
-            const { dx, dy } = gestureState;
-            setPosition1(prevPosition1 => ({
-                x: prevPosition1.x + dx * scale1,
-                y: prevPosition1.y + dy * scale1,
-            }));
-        },
-        onPanResponderTerminationRequest: () => false,
-    });
-
-    const panResponder2 = PanResponder.create({
-        onMoveShouldSetPanResponder: (_, gestureState) => {
-            if (gestureState.numberActiveTouches > 1) {
-                return false;
-            }
-            return true;
-        },
-        onPanResponderMove: (_, gestureState) => {
-            const { dx, dy } = gestureState;
-            setPosition2(prevPosition2 => ({
-                x: prevPosition2.x + dx * scale2,
-                y: prevPosition2.y + dy * scale2,
-            }));
-        },
-        onPanResponderTerminationRequest: () => false,
-    });
 
     return (
-        <>
-            <View style={{ flexDirection: 'column', justifyContent: 'space-between', flex: 1 }}>
-                <View
-                    style={[
-                        styles(theme).row,
-                        { borderBottomWidth: borderWidth, borderBottomColor: theme.colors.outline },
-                    ]}
-                >
-                    {isDisable ?
-                        templateData.some(item => item.name === 'p1') &&
-                        <Svg height={170}
-                            width={Dimensions.get('window').width}
-                            style={[styles(theme).row, { borderBottomWidth: borderWidth, borderBottomColor: theme.colors.outline }]}>
-                            <SvgImage
-                                href={templateData.find(item => item.name === 'p1').source}
-                                preserveAspectRatio="xMidYMid slice"
-                                clipPath="url(#clip)"
-                                x={`${((templateData.find(item => item.name === 'p1').x / Dimensions.get('window').width) * 100)}%`}
-                                y={`${((templateData.find(item => item.name === 'p1').y / 170) * 100)}%`}
-                                width={templateData.find((item) => item.name === 'p1').width * scaleFactor1 * templateData.find(item => item.name === 'p1').scale}
-                                height={templateData.find((item) => item.name === 'p1').height * scaleFactor1 * templateData.find(item => item.name === 'p1').scale}
-                            />
-                        </Svg>
-                        :
-                        <PinchGestureHandler
-                            ref={pinchRef1}
-                            onGestureEvent={handlePinch1}
-                            onHandlerStateChange={event => {
-                                if (event.nativeEvent.oldState === State.ACTIVE) {
-                                    handlePinchEnd1();
-                                }
-                            }}
-                        >
-                            <Animated.View>
-                                {templateData.some((item) => item.name === 'p1') ? (
-                                    <Svg
-                                        height={170}
-                                        width={Dimensions.get('window').width}
-                                        style={[
-                                            styles(theme).row,
-                                            { borderBottomWidth: borderWidth, borderBottomColor: theme.colors.outline },
-                                        ]}>
-                                        <SvgImage
-                                            href={templateData.find((item) => item.name === 'p1').source}
-                                            preserveAspectRatio="xMidYMid slice"
-                                            clipPath="url(#clip)"
-                                            x={`${((position1.x / Dimensions.get('window').width) * 100)}%`}
-                                            y={`${((position1.y / 170) * 100)}%`}
-                                            width={templateData.find((item) => item.name === 'p1').width * scaleFactor1 * scale1}
-                                            height={templateData.find((item) => item.name === 'p1').height * scaleFactor1 * scale1}
-                                            {...panResponder1.panHandlers}
-                                        />
-                                    </Svg>
-                                ) : (
-                                    <MMIcon
-                                        iconName={'plus-circle'}
-                                        style={styles(theme).imagePickerButton}
-                                        onPress={() => onPickImage('p1', 'img')}
-                                    />
-                                )}
-                            </Animated.View>
-                        </PinchGestureHandler>
-                    }
-                </View>
-
-                <View style={styles(theme).row}>
-                    {isDisable ?
-                        templateData.some(item => item.name === 'p2') &&
-                        <Svg height={170}
-                            width={Dimensions.get('window').width}
-                            style={[styles(theme).row, { borderBottomWidth: borderWidth, borderBottomColor: theme.colors.outline }]}>
-                            <SvgImage
-                                href={templateData.find(item => item.name === 'p2').source}
-                                preserveAspectRatio="xMidYMid slice"
-                                clipPath="url(#clip)"
-                                x={`${((templateData.find(item => item.name === 'p2').x / Dimensions.get('window').width) * 100)}%`}
-                                y={`${((templateData.find(item => item.name === 'p2').y / 170) * 100)}%`}
-                                width={templateData.find((item) => item.name === 'p2').width * scaleFactor2 * templateData.find(item => item.name === 'p2').scale}
-                                height={templateData.find((item) => item.name === 'p2').height * scaleFactor2 * templateData.find(item => item.name === 'p2').scale}
-                            />
-                        </Svg>
-                        :
-                        <PinchGestureHandler
-                            ref={pinchRef2}
-                            onGestureEvent={handlePinch2}
-                            onHandlerStateChange={event => {
-                                if (event.nativeEvent.oldState === State.ACTIVE) {
-                                    handlePinchEnd2();
-                                }
-                            }}
-                        >
-                            <Animated.View>
-                                {templateData.some((item) => item.name === 'p2') ? (
-                                    <Svg
-                                        height={170}
-                                        width={Dimensions.get('window').width}
-                                        style={[
-                                            styles(theme).row,
-                                            { borderBottomWidth: borderWidth, borderBottomColor: theme.colors.outline },
-                                        ]}>
-                                        <SvgImage
-                                            href={templateData.find((item) => item.name === 'p2').source}
-                                            preserveAspectRatio="xMidYMid slice"
-                                            clipPath="url(#clip)"
-                                            x={`${((position2.x / Dimensions.get('window').width) * 100)}%`}
-                                            y={`${((position2.y / 170) * 100)}%`}
-                                            width={templateData.find((item) => item.name === 'p2').width * scaleFactor2 * scale2}
-                                            height={templateData.find((item) => item.name === 'p2').height * scaleFactor2 * scale2}
-                                            {...panResponder2.panHandlers}
-                                        />
-                                    </Svg>
-                                ) : (
-                                    <MMIcon
-                                        iconName={'plus-circle'}
-                                        style={styles(theme).imagePickerButton}
-                                        onPress={() => onPickImage('p2', 'img')}
-                                    />
-                                )}
-                            </Animated.View>
-                        </PinchGestureHandler>}
-                </View>
-            </View>
-            {!templateData.some((item) => item.name === 'p1') || isDisable ? null : (
-                <MMIcon
-                    iconName={'camera'}
-                    style={{
-                        position: 'absolute',
-                        top: 0,
-                    }}
-                    onPress={() => onPickImage('p1', 'img')}
-                />
-            )}
-            {!templateData.some((item) => item.name === 'p2') || isDisable ? null : (
-                <MMIcon
-                    iconName={'camera'}
-                    style={{
-                        position: 'absolute',
-                        bottom: 2,
-                    }}
-                    onPress={() => onPickImage('p2', 'img')}
-                />
-            )}
-        </>
+        <View style={styles(theme).column}>
+            {renderImageBox('p1', scaleFactor1, pageId, { borderBottomWidth: 1, borderBottomColor: theme.colors.outline })}
+            {renderImageBox('p2', scaleFactor2, pageId)}
+        </View>
     );
 };
 
-const styles = (theme) =>
-    StyleSheet.create({
-        row: {
-            alignItems: 'center',
-            justifyContent: 'center',
-            flex: 1,
-            paddingVertical: 1,
-        },
-        imagePickerButton: {
-            padding: MMConstants.paddingLarge,
-            borderRadius: 50,
-        },
-        image: {
-            width: '100%',
-            height: 170,
-        },
-    });
+const styles = (theme) => StyleSheet.create({
+    column: {
+        flex: 1,
+        flexDirection: 'column', // main axis
+    },
+    row: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        paddingVertical: 1,
+
+    },
+    imagePickerButton: {
+        padding: MMConstants.paddingLarge,
+        borderRadius: 50,
+    }
+});
 
 export default Row2;
