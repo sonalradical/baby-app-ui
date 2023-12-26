2969f69b479fb2e1dc044654f67d7505e3c4fe65
Nupur Shah <sonal@radicalrack.com>
2023-12-26 12:54:27 +0530

    banner for order now UI

--- src/screens/banners/OrderNowBanner.js
+++ src/screens/banners/OrderNowBanner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
 import { useTheme } from 'react-native-paper';
 import * as _ from 'lodash';
 
@@ -12,11 +12,26 @@ const OrderNowBanner = ({ onPress }) => {
 
     return (
         <View style={{ marginVertical: MMConstants.marginMedium }}>
-            <MMSurface style={{ alignItems: 'center', borderRadius: 20 }} margin={[0, 0, 0, 0]}>
-                <Text style={theme.fonts.headlineMedium}>Order your book now.</Text>
-                <MMButton mode="contained" label={'Order Now'} onPress={onPress} />
+            <MMSurface style={{ borderRadius: 10, backgroundColor: theme.colors.secondary, flexDirection: 'row' }} margin={[0, 0, 0, 0]}>
+                <Image
+                    resizeMode="contain"
+                    source={require('../../assets/images/chapter/banner.png')}
+                    style={styles(theme).image}
+                />
+                <View style={{ paddingLeft: 30 }}>
+                    <Text style={[theme.fonts.headlineMedium, { color: theme.colors.secondaryContainer }]}>Order your book now.</Text>
+                    <MMButton mode="contained" label={'Order Now'} onPress={onPress} />
+                </View>
             </MMSurface>
         </View>
     );
 }
+const styles = (theme) => StyleSheet.create({
+    image: {
+        width: Dimensions.get('window').width / 4,
+        height: Dimensions.get('window').height / 8,
+        padding: 10,
+    },
+});
+
 export default OrderNowBanner;
