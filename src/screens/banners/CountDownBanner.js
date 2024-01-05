642e96cbf2d64e4e5af5539d590700b4ad052d80
Sonal Shah <sonal@radicalrack.com>
2024-01-05 15:18:29 +0530

    Added count down banner

--- src/screens/banners/CountDownBanner.js
+++ src/screens/banners/CountDownBanner.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { Dimensions, Image, ImageBackground, StyleSheet, View } from 'react-native';
-import { Card, Text, useTheme } from 'react-native-paper';
 import * as _ from 'lodash';
+import React, { useEffect, useState } from 'react';
+import { Dimensions, ImageBackground, StyleSheet, View } from 'react-native';
+import { Card, Text, useTheme } from 'react-native-paper';
 import { useSelector } from 'react-redux';
 
 import MMConstants from '../../helpers/Constants';
 import MMUtils from '../../helpers/Utils';
-import MMSurface from '../../components/common/Surface';
 
 
 const CountDownBanner = ({ }) => {
@@ -33,62 +32,28 @@ const CountDownBanner = ({ }) => {
     }, [userDetail.dueDate]);
 
     return (
-        // <View style={{ marginHorizontal: MMConstants.marginLarge, marginVertical: MMConstants.marginMedium }}>
-        //     <MMSurface style={{ alignItems: 'center', borderRadius: 20 }} margin={[0, 0, 0, 0]}>
-        //         <Text style={theme.fonts.headlineMedium}>Only</Text>
-        //         <View style={{ flexDirection: 'row' }}>
-        //             {state.months > 0 &&
-        //                 <Card style={{
-        //                     backgroundColor: theme.colors.primary, padding: MMConstants.paddingLarge,
-        //                     margin: MMConstants.marginMedium
-        //                 }}>
-        //                     <Text style={[theme.fonts.titleLarge, { textAlign: 'center' }]}>{state.months}</Text>
-        //                     <Text style={{ color: theme.colors.secondaryContainer }}>{state.months === 1 ? 'month' : 'months'}</Text>
-        //                 </Card>}
-        //             {state.weeks > 0 &&
-        //                 <Card style={{
-        //                     backgroundColor: theme.colors.primary, padding: MMConstants.paddingLarge,
-        //                     margin: MMConstants.marginMedium
-        //                 }}>
-        //                     <Text style={[theme.fonts.titleLarge, { textAlign: 'center' }]}>{state.weeks}</Text>
-        //                     <Text style={{ color: theme.colors.secondaryContainer }}>{state.weeks === 1 ? 'week' : 'weeks'}</Text>
-        //                 </Card>}
-        //             {state.days > 0 &&
-        //                 <Card style={{
-        //                     backgroundColor: theme.colors.primary, padding: MMConstants.paddingLarge,
-        //                     paddingHorizontal: 20, margin: MMConstants.marginMedium
-        //                 }}>
-        //                     <Text style={[theme.fonts.titleLarge, { textAlign: 'center' }]}>{state.days}</Text>
-        //                     <Text style={{ color: theme.colors.secondaryContainer }}>{state.days === 1 ? 'day' : 'days'}</Text>
-        //                 </Card>}
-        //         </View>
-        //         <Text style={theme.fonts.headlineMedium}>to arrive</Text>
-        //         <Text> Due date {dueDate}</Text>
-        //     </MMSurface>
-        // </View>
-
-        <View style={{ marginVertical: MMConstants.marginLarge }}>
+        <Card style={{ marginVertical: MMConstants.marginLarge, backgroundColor: theme.colors.primary }}>
 
             <ImageBackground
-                resizeMode="cover"
-                source={require('../../assets/images/coundown-banner1.png')}
+                resizeMode="contain"
+                source={require('../../assets/images/countdown_banner.png')}
                 style={styles(theme).image}
             >
 
-                <Text style={[theme.fonts.titleLarge]}>Baby Countdown</Text>
-                <View style={{ flexDirection: 'row', }}>
+                <Text style={[theme.fonts.titleLarge, { paddingHorizontal: 12, paddingVertical: 8 }]}>Baby Countdown</Text>
+                <View style={{ flexDirection: 'row', paddingHorizontal: 12 }}>
                     {state.weeks > 0 &&
                         <View style={[styles(theme).card]}>
-                            <Text style={[theme.fonts.titleLarge, { textAlign: 'center' }]}>{state.weeks}</Text>
-                            <Text style={{ color: theme.colors.secondaryContainer, textAlign: 'center' }}>
+                            <Text style={[theme.fonts.titleMedium, { textAlign: 'center' }]}>{state.weeks}</Text>
+                            <Text style={styles(theme).subText}>
                                 {state.weeks === 1 ? 'Week' : 'Weeks'}
                             </Text>
                         </View>
                     }
                     {state.days > 0 &&
                         <View style={[styles(theme).card]}>
-                            <Text style={[theme.fonts.titleLarge, { textAlign: 'center' }]}>{state.days}</Text>
-                            <Text style={{ color: theme.colors.secondaryContainer, textAlign: 'center' }}>
+                            <Text style={[theme.fonts.titleMedium, { textAlign: 'center' }]}>{state.days}</Text>
+                            <Text style={styles(theme).subText}>
                                 {state.days === 1 ? 'Day' : 'Days'}
                             </Text>
                         </View>
@@ -96,18 +61,18 @@ const CountDownBanner = ({ }) => {
                     }
                     {state.months > 0 &&
                         <View style={[styles(theme).card]}>
-                            <Text style={[theme.fonts.titleLarge, { textAlign: 'center' }]}>{state.months}</Text>
-                            <Text style={{ color: theme.colors.secondaryContainer, textAlign: 'center' }}>
+                            <Text style={[theme.fonts.titleMedium, { textAlign: 'center' }]}>{state.months}</Text>
+                            <Text style={styles(theme).subText}>
                                 {state.months === 1 ? 'Month' : 'Months'}
                             </Text>
                         </View>
                     }
                 </View>
-                <Text style={[theme.fonts.titleMedium, { textAlign: 'center', color: 'white' }]}>until our baby is born</Text>
+                <Text style={[theme.fonts.titleMedium, { color: 'white', paddingHorizontal: 12, paddingVertical: 8 }]}>until our baby is born</Text>
 
             </ImageBackground>
 
-        </View>
+        </Card >
 
     );
 }
@@ -115,15 +80,24 @@ const CountDownBanner = ({ }) => {
 const styles = (theme) => StyleSheet.create({
 
     card: {
-        marginHorizontal: 5,
+        marginRight: 5,
+        width: '25%',
         paddingHorizontal: 20,
-        backgroundColor: 'rgba(0,0,0, 0.7)', // Transparent white background
+        paddingVertical: 8,
+        borderRadius: 8,
+        backgroundColor: 'rgba(255, 255, 255, 0.3)'
+        , // Transparent white background
     },
     image: {
-        width: 'auto',
-        height: Dimensions.get('window').height / 4,
-
+        width: Dimensions.get('window').width - 24,
+        // height: 180
     },
+    subText: {
+        // color: 'white',
+        fontSize: 8,
+        fontWeight: 700,
+        textAlign: 'center'
+    }
 
 });
 export default CountDownBanner;
